4c114490808520ed8eefe51cc0b6e4760d7524c4
Md. Arafat Kabir <mdarafatkabir95@gmail.com>
2025-05-15 19:35:05 +0600

    create

--- myapp/settings.py
+++ myapp/settings.py
@@ -3,8 +3,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'worldspecializedhospital@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'titd baxd vdys pgcq'  # Replace with your app password
+EMAIL_HOST_USER = 'ABC@gmail.com'  # Replace with your email
+EMAIL_HOST_PASSWORD = 'aaaa bbb ccc ddd'  # Replace with your app password
 
 
 TIME_ZONE = 'Asia/Dhaka'
